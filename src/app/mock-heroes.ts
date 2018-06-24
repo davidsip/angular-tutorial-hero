7df788a6839f1c1f8f0b2a33d15837f5f5087ade
davidsip <davidsip@outlook.com>
2018-06-23 22:10:58 -0700

    first commit1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'John'},
+  {id: 12, name: 'Dave'},
+  {id: 13, name: 'Bob'},
+  {id: 14, name: 'Sally'},
+  {id: 15, name: 'Mary'},
+  {id: 16, name: 'Dan'},
+  {id: 17, name: 'Beth'},
+  {id: 18, name: 'Jane'},
+  {id: 19, name: 'Carrie'},
+  {id: 20, name: 'Taylor'},
+];
